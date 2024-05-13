522b9a555d09fe570e71900f23752c19925ad18c
jDavidAA <46817825+jDavidAA@users.noreply.github.com>
2024-05-13 10:12:12 -0600

    Update 5-2-1.sh

--- 5-2-1.sh
+++ 5-2-1.sh
@@ -3,6 +3,7 @@
 # Remediation is applicable only in certain platforms
 if [ ! -f /.dockerenv ] && [ ! -f /run/.containerenv ]; then
 
+chown root:root /etc/ssh/sshd_config
 chmod u-xs,g-xwrs,o-xwrt /etc/ssh/sshd_config
 
 else
